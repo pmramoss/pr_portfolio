2c2b00128d5d1b8740186b29205791c191babccd
Pedro Lucas Ramos <pedromcramos13@gmail.com>
2023-04-24 03:52:42 -0300

    att

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -1,6 +1,7 @@
 import styles from "./Aboutme.module.scss";
 import img from "../../assets/sob1.png";
 import { GiAchievement } from "react-icons/gi"
+import { ImLinkedin, ImGithub } from "react-icons/im"
 import { IContent } from "../../types/content";
 
 interface Props {
@@ -17,7 +18,15 @@ export default function AboutMe({content}: Props){
                     <h3>{content.Aboutme_Title}</h3>
                     <p className={styles.aboutme__info__text}>{content.Aboutme_text1}<strong>{content.Aboutme_text2}</strong>{content.Aboutme_text3}</p>
                     <div className={styles.aboutme__info__certifications}>
-                         <a href="/certificate">{content.Aboutme_link} <GiAchievement /></a>
+                         <a href="#certificate">{content.Aboutme_link} <GiAchievement /></a>
+                    </div>
+                    <div className={styles.links}>
+                         <div>
+                              <a href="https://github.com/pmramoss" target="_blank"><ImGithub/>GitHub</a>
+                         </div>
+                         <div>
+                              <a href="www.linkedin.com/in/pedro-lucas-ramos-557189217" target="_blank"><ImLinkedin/>LinkedIn</a>
+                         </div>
                     </div>
                </div>
           </div>
