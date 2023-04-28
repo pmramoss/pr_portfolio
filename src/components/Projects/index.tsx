e30b46ab0380c8fae8df0d6852b0f535aa2b0ac5
Pedro Lucas Ramos <pedromcramos13@gmail.com>
2023-04-28 15:54:59 -0300

    att links

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -13,7 +13,7 @@ export default function Projects({content}: Props){
                <div className={styles.projects__title}>
                     <h2>{content.Projects_title}</h2>
                </div>
-               <Container image={aluroni} name="Aluroni" descript={content.Projects_descript1} tag1="React" tag2="Sass" github="https://github.com/pmramoss" link="https://www.google.com/"/>
+               <Container image={aluroni} name="Aluroni" descript={content.Projects_descript1} tag1="React" tag2="Sass" github="https://github.com/pmramoss/pr_aluroni" link="https://pr-aluroni.vercel.app/"/>
                <Container image={chatgpt3} name="ChatGPT3" descript={content.Projects_descript2} tag1="React" tag2="Sass" github="https://github.com/pmramoss/ChatGPT3" link="https://chat-gpt-3-ashen.vercel.app/"/>
                <Container image={form} name="Form" descript={content.Projects_descript3} tag1="React" tag2="Styled Components" github="https://github.com/pmramoss/signup_wvr" link="https://signup-wvr.vercel.app/"/>
                <Container image={portfolio} name="Portfolio" descript={content.Projects_descript4} tag1="React" tag2="Sass" github="https://github.com/pmramoss/pr_portfolio" link="https://pr-portfolio-gamma.vercel.app/"/>
